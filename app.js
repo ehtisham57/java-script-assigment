c3598a9c276af883c6b579cdafe1017d4c3c5605
ehtisham57 <81214666+ehtisham57@users.noreply.github.com>
2021-11-04 13:13:43 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,96 @@
+//Question 1
+// var studentName = [];
+
+//Question 2
+
+// var studentName = []
+
+//Question 3
+
+// var city1 = "Karachi";
+// alert("welcome to city of light " + city1);
+
+
+// Question 4
+
+// var myNum = 20;
+// alert("My Num: " + myNum);
+
+// Question 5
+
+// var arr = true;
+// var arr1 = false;
+// console.log(arr1)
+
+//Question 6
+
+// var arr = ["My Num", 50, true, false];
+// console.log(arr);
+
+
+//Question 7
+
+// var education = ["1) SSC", "2) HSC", "3) BSC", "4) BS", "5) BCOM", "6) MS", "7) M.Phil", "8) PhD"];
+// for (var a = 0; a <= 7; a++) {
+//     document.write(education[a] + "<br><br>")
+// }
+
+//Question 8
+
+// var studentName = ["Michel ", "jhon ", "tony "];
+// var Marks = [320, 230, 480]
+//     // var total = 500
+// for (var a = 0; a <= 2; a++) {
+//     document.write("Score of " +
+//         studentName[a] + "is:" + Marks[a] + ". percentage: " + "" + Marks[a] / 500 * 100 + " %<br><br> ")
+// }
+
+
+
+//Question 9
+
+
+
+//Question 10
+
+
+// var a = [320, 230, 480, 120];
+// a.sort();
+// document.write(a);
+
+//Question 11
+
+// var cities = ["Karachi", "Multan", "Peshawar", "Lahore"]
+// var selectedCities = ["Karachi", "Multan"]
+// document.write("Cities List : <br/>" + cities + "<br/><br/><br/><br/>" + "selected Cities List: " + "<br/>" + selectedCities)
+
+
+//Question 12
+
+
+// var arr = ["This", "is", "my", "car"]
+// var a = arr.join(" ");
+// document.write(a);
+
+//Question 13
+
+
+// var b = ["Keyboard", "mouse", "printer", "monitor"]
+// for (var a = 0; a <= 3; a++)
+//     document.write("Out <br/>" + b[a] + "<br/><br/>")
+
+
+//Question 14
+
+
+// var b = ["Keyboard", "mouse", "printer", "monitor"]
+// b.reverse();
+// for (var a = 0; a <= 3; a++)
+
+//     document.write("Out <br/>" + b[a] + "<br/><br/>")
+
+
+//Question 15
+
+var Phone = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", "&", "Haier"]
+document.write(Phone)
